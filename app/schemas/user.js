427a05ed506210bcdc78aab94c889ac43cc0446d
vtrust <vtrust32@gmail.com>
2017-11-21 21:18:26 +0800

    per-head

--- app/schemas/user.js
+++ app/schemas/user.js
@@ -27,8 +27,8 @@ var UserSchema = new mongoose.Schema({
             default:Date.now()
         }
     },
-    avatar: {type:String, default:'http://img1.mukewang.com/575cd43d0001242904190419-200-200.jpg'},
-    background: {type:String, default:'http://www.deskcar.com/desktop/else/2009221114947/12.jpg'},
+    avatar: {type:String, default:'img\\defaultAvatar.jpg'},
+    background: {type:String, default:'img\\defaultBackground.jpg'},
     gender: { type: String},
     desc: String ,
     signature: String,
