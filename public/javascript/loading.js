b3eb54e35f8f9a63fb2f56bba06ff14146e6480e
vtrust <vtrust32@gmail.com>
2017-11-22 08:58:27 +0800

    add loading animotion

---
+++ public/javascript/loading.js
@@ -0,0 +1,14 @@
+ window.addEventListener("load", function () {
+     console.log('loading fadOut')
+     $('#loading').fadeOut(1000);
+ });
+
+//  $(window).load(function() {
+//     console.log('loading fadOut')
+//     $('#loading').fadeOut("slow");
+// })
+
+// window.addEventListener("load", function() {
+//     var loading = document.getElementById("loading");
+//     document.body.removeChild(loading);
+// });
