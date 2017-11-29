c7f85bedcfc814ca553fb68a12bb35de573b5897
vtrust <vtrust32@gmail.com>
2017-11-29 09:21:14 +0800

    post timeline

--- app/schemas/article.js
+++ app/schemas/article.js
@@ -2,46 +2,94 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 var ArticleSchema = new mongoose.Schema({
-    title:String,
-    meta:{
-        createAt:{
-            type:Date,
-            default:Date.now()
+    title: String,
+    cover:{
+        type:String,
+        default: 'img\\defaultArticleCover.jpg'        
+    },
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
         },
-        updateAt:{
-            type:Date,
-            default:Date.now()
+        updateAt: {
+            type: Date,
+            default: Date.now()
         }
     },
-    author: { type: ObjectId, ref:'User' },
-    desc: { type: 'string' },
-    content:String,
-    readc: Number,
-    commentc:Number,
-    applausec:Number
+    author: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    desc: {
+        type: String
+    },
+    content: String,
+    readc: {
+        type:Number,
+        default:0
+    },
+    commentc: {
+        type:Number,
+        default:0
+    },
+    applausec:{
+        type:Number,
+        default:0
+    }
 });
 
 //静态方法，在model层可使用这个方法，不需要new 实例化
-ArticleSchema.statics={
-    fetch:function(cb){
+ArticleSchema.statics = {
+    fetch: function (cb) {
         return this
             .find({})
             .sort('meta.updateAt')
-            .exec(cb)//执行callback
+            .exec(cb) //执行callback
     },
-    findById:function(id,cb){
+    findById: function (id, cb) {
         return this
-            .findOne({_id:id})
-            .exec(cb)//执行callback
+            .findOne({
+                _id: id
+            })
+            .exec(cb) //执行callback
+    },
+    findLimitByUserId: function (author, cb) {
+        return this
+            .find({
+                author: author
+            })
+            .sort({'meta.updateAt':-1})
+            .limit(10)
+            .exec(cb)
+    },
+    findByUserId: function (author, cb) {
+        return this
+            .find({
+                author: author
+            })
+            .sort({'meta.createAt':-1})
+            .exec(cb)
     }
 };
-ArticleSchema.pre('save',function(next){
-    var article=this;
-    if(this.isNew){
-        this.meta.createAt=this.meta.updateAt=Date.now()
-    }else{
-        this.meta.updateAt=Date.now()
+ArticleSchema.pre('save', function (next) {
+    var article = this;
+    if (this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now()
+    } else {
+        this.meta.updateAt = Date.now()
     }
     next();
 });
-module.exports=ArticleSchema;
+
+//实例方法
+ArticleSchema.methods = {
+    findByUserId: function (author, cd) {
+        return this.find({
+            'author': author
+        }).sort({
+            _id: -1
+        }).exec(cb)
+    }
+};
+module.exports = ArticleSchema;
